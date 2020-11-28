1f4c2a5a4681a9b599f3a8914eb20f11165b8227
dmmaia <david_mmaia@hotmail.com>
2020-11-28 16:04:57 -0300

    Vibrate function added

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Vibration,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 import IconAw from 'react-native-vector-icons/FontAwesome';
+import IconEnt from 'react-native-vector-icons/Entypo';
 
 const Timer = () => {
   const [minutes, setMinutes] = useState(25);
@@ -35,9 +42,10 @@ const Timer = () => {
         setZeroForMinutes('');
       }
       setSeconds(0);
-
+      Vibration.vibrate();
       setTimeout(() => {
         setRest(rest === true ? false : true);
+        Vibration.cancel();
       }, 1000);
     }
 
@@ -50,6 +58,7 @@ const Timer = () => {
     setSeconds(0);
     setRest(true);
     setZeroForSeconds('0');
+    setZeroForMinutes('');
   }
 
   return (
@@ -59,6 +68,7 @@ const Timer = () => {
           {zeroforMinutes}
           {minutes}
         </Text>
+        <Text style={styles.equalSinal}>:</Text>
         <Text style={styles.secondsField}>
           {zeroforSeconds}
           {seconds}
@@ -69,18 +79,26 @@ const Timer = () => {
         <TouchableOpacity
           style={styles.pauseButton}
           onPress={() => setStatus(false)}>
-          <Icon name="pausecircle" size={70} color="orange" />
+          <Icon
+            name={status === true ? 'pause' : 'pausecircle'}
+            size={70}
+            color="white"
+          />
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.stopButton}
           onPress={() => handleStop()}>
-          <IconAw name="stop" size={70} color="red" />
+          <IconAw name="stop" size={70} color="#660000" />
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.startButton}
           onPress={() => setStatus(true)}>
-          <Icon name="play" size={70} color="green" />
+          {status === true ? (
+            <Icon name="play" size={70} color="white" />
+          ) : (
+            <IconEnt name="controller-play" size={70} color="white" />
+          )}
         </TouchableOpacity>
       </View>
     </View>
@@ -105,14 +123,20 @@ const styles = StyleSheet.create({
   boxTimer: {
     flexDirection: 'row',
   },
+  equalSinal: {
+    color: 'white',
+    fontSize: 80,
+    marginLeft: 10,
+  },
   startButton: {
-    marginLeft: 50,
+    marginLeft: 25,
   },
   pauseButton: {
-    margin: 0,
+    marginLeft: 0,
   },
   stopButton: {
-    marginLeft: 50,
+    marginLeft: 45,
+    marginTop: 200,
   },
   buttonText: {
     color: 'white',
